4366d25821a3df25df04a2a3406c567a56ea3f41
Longitude-2877 <longitude-2877@users.noreply.github.com>
2026-08-12 21:19:14 +0530

    Set compileSdk 36 (required by flutter_volume_controller 2.x)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.elderlauncher"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
